ffd3c402e339f95f410f362cf52d5f027dc247bf
Lindenbaum <tuaca@Lindenbaumui-MacBookAir.local>
2023-04-28 21:28:51 +0900

    basic project setting

---
+++ src/renderer/index.js
@@ -0,0 +1,38 @@
+const ffmpeg = require('fluent-ffmpeg');
+
+function encodeVideo(inputFile, outputFile, codec, progressCallback) {
+    return new Promise((resolve, reject) => {
+        ffmpeg(inputFile)
+            .output(outputFile)
+            .videoCodec(codec)
+            .on('progress', progress => progressCallback(progress.percent))
+            .on('end', resolve)
+            .on('error', reject)
+            .run();
+    });
+}
+
+const inputFile = document.getElementById('inputFile');
+const outputFile = document.getElementById('outputFile');
+const codec = document.getElementById('codec');
+const encodeForm = document.getElementById('encodeForm');
+const progressBar = document.getElementById('progressBar');
+
+encodeForm.addEventListener('submit', async event => {
+    event.preventDefault();
+
+    const inputFilePath = inputFile.files[0].path;
+    const outputFilePath = outputFile.value;
+    const selectedCodec = codec.value;
+
+    progressBar.value = 0;
+
+    try {
+        await encodeVideo(inputFilePath, outputFilePath, selectedCodec, progress => {
+            progressBar.value = progress;
+        });
+        alert('Encoding completed successfully!');
+    } catch (error) {
+        alert(`An error occurred during encoding: ${error.message}`);
+    }
+});
